8587c389be990c66a0e5c3a05d78cf020013490e
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 01:42:10 -0300

    feat: implementado Command, Handler e UseCase para Create de User

---
+++ MeetingRoomScheduling.Application/Interfaces/ICreateUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using MeetingRoomScheduling.Application.Requests;
+using MeetingRoomScheduling.Domain.Entities;
+
+namespace MeetingRoomScheduling.Application.Interfaces
+{
+    public interface ICreateUserUseCase
+    {
+        Task<User> Execute(CreateUserRequest request);
+    }
+}
---
+++ MeetingRoomScheduling.Application/UseCases/User/CreateUserUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using MeetingRoomScheduling.Application.Commands.User;
+using MeetingRoomScheduling.Application.Interfaces;
+using MeetingRoomScheduling.Application.Requests;
+using Tourmine.Tournament.Application.UseCases;
+
+namespace MeetingRoomScheduling.Application.UseCases.User
+{
+    public class CreateUserUseCase : BaseUseCase, ICreateUserUseCase
+    {
+        public CreateUserUseCase(IMediator mediator) : base(mediator)
+        {}
+
+        public async Task<Domain.Entities.User> Execute(CreateUserRequest request)
+        {
+            return await mediator.Send(new CreateUserCommand(request));
+        }
+    }
+}
