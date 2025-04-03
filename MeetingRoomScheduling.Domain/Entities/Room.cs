9ed1d4a59d66405cfd489cec19d4acf9bf31479d
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 19:03:39 -0300

    feat: implementado construtor na entidade Room

--- MeetingRoomScheduling.Domain/Entities/Room.cs
+++ MeetingRoomScheduling.Domain/Entities/Room.cs
@@ -6,5 +6,12 @@
         public string Name { get; set; }
         public int MaximumPeopleCapacity { get; set; }
         public virtual List<Booking> Bookings { get; set; }
+
+        public Room(string name, int maximumPeopleCapacity)
+        {
+            Name = name;
+            MaximumPeopleCapacity = maximumPeopleCapacity;
+            Bookings = new List<Booking>();
+        }
     }
 }
