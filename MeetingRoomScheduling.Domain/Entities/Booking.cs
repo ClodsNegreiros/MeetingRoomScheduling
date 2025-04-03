c2d0385e77c52d85ca0e532f953793b5dd83d18d
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 20:34:57 -0300

    feat: implementado construtor na classe Booking

--- MeetingRoomScheduling.Domain/Entities/Booking.cs
+++ MeetingRoomScheduling.Domain/Entities/Booking.cs
@@ -11,8 +11,15 @@ namespace MeetingRoomScheduling.Domain.Entities
         public User User{ get; set; }
         public DateTime BookingStartDate { get; set; }
         public DateTime BookingEndDate { get; set; }
-
-        // TODO: ADD STATUS PROPERTY - ativa/cancelada
         public EBookingStatus Status { get; set; }
+
+        public Booking(int roomId, int userId, DateTime bookingStartDate, DateTime bookingEndDate, EBookingStatus status)
+        {
+            RoomId = roomId;
+            UserId = userId;
+            BookingStartDate = bookingStartDate;
+            BookingEndDate = bookingEndDate;
+            Status = status;
+        }
     }
 }
