862ff663220c0b9ef09b44222c0fb0459b226e0f
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 15:04:40 -0300

    feat: implementado authController utilizando loginUserUseCase

---
+++ MeetingRoomScheduling/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using MeetingRoomScheduling.Application.Dtos.Auth;
+using Microsoft.AspNetCore.Mvc;
+using MeetingRoomScheduling.Application.Interfaces.Auth;
+using MeetingRoomScheduling.Application.Requests.Auth;
+
+namespace MeetingRoomScheduling.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("v1/authenticate")]
+        public async Task<ActionResult<UserToken>> Login(
+            [FromBody] LoginUserRequest request,
+            [FromServices] ILoginUserUseCase useCase)
+        {
+            var result = await useCase.Execute(request); 
+
+            return Ok(result);
+        }
+    }
+}
