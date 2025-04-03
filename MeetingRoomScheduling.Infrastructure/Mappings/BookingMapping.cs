c3c785e2a689e36f645c745275a2dab43232bd9e
ClodsNegreiros <clodsn@gmail.com>
2025-04-02 23:37:57 -0300

    feat: implementado mapping das entidades Booking, Room e User

---
+++ MeetingRoomScheduling.Infrastructure/Mappings/BookingMapping.cs
@@ -0,0 +1,21 @@
+﻿using MeetingRoomScheduling.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeetingRoomScheduling.Infrastructure.Mappings
+{
+    public class BookingMapping : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(booking => booking.Id);
+
+            builder.Property(booking => booking.Id);
+            builder.Property(booking => booking.UserId);
+            builder.Property(booking => booking.RoomId);
+            builder.Property(booking => booking.BookingStartDate);
+            builder.Property(booking => booking.BookingEndDate);
+        }
+    }
+}
