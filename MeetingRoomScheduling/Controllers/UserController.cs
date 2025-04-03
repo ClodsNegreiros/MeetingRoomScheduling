6700ca4b88ec06507e0b2e148737ff9ff8edb75b
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 01:43:57 -0300

    feat: implementado controller de user com método post

---
+++ MeetingRoomScheduling/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using MeetingRoomScheduling.Application.Interfaces;
+using MeetingRoomScheduling.Application.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingRoomScheduling.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("v1/create")]
+        public async Task<IActionResult> Create([FromBody] CreateUserRequest request, [FromServices] ICreateUserUseCase useCase)
+        {
+            var result = await useCase.Execute(request);
+            return Ok(result);
+        }
+    }
+}
