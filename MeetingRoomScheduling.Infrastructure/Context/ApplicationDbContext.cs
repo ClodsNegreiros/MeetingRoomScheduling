b3231fa0b3168a54c7019ae65df209bd7fa4b7f2
ClodsNegreiros <clodsn@gmail.com>
2025-04-02 23:38:54 -0300

    feat: implementado OnModelCreating para para configuração das entidades no ApplicationDbContext

--- MeetingRoomScheduling.Infrastructure/Context/ApplicationDbContext.cs
+++ MeetingRoomScheduling.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MeetingRoomScheduling.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeetingRoomScheduling.Infrastructure.Context
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+        {}
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookingMapping());
+            modelBuilder.ApplyConfiguration(new RoomMapping());
+            modelBuilder.ApplyConfiguration(new UserMapping());
         }
     }
 }
