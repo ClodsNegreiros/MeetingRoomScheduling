b816aef90b33c8efebf21696a9de08fa76721683
ClodsNegreiros <clodsn@gmail.com>
2025-04-02 23:23:51 -0300

    feat: implementado entidades Booking, Room e User

---
+++ MeetingRoomScheduling.Domain/Entities/Booking.cs
@@ -0,0 +1,13 @@
+﻿namespace MeetingRoomScheduling.Domain.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+        public int UserId { get; set; }
+        public User User{ get; set; }
+        public DateTime BookingStartDate { get; set; }
+        public DateTime BookingEndDate { get; set; }
+    }
+}
---
+++ MeetingRoomScheduling.Domain/Entities/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetingRoomScheduling.Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaximumPeopleCapacity { get; set; }
+    }
+}
