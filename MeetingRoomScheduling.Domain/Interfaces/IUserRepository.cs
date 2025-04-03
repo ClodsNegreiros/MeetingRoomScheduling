0e80abf95d432cd06db3e89709814c34486c5c46
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 01:39:44 -0300

    feat: implementado repository de user com Create, Update e Delete

---
+++ MeetingRoomScheduling.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MeetingRoomScheduling.Domain.Entities;
+
+namespace MeetingRoomScheduling.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task DeleteAsync(User user);
+        Task UpdateAsync(User user);
+    }
+}
---
+++ MeetingRoomScheduling.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using MeetingRoomScheduling.Domain.Entities;
+using MeetingRoomScheduling.Domain.Interfaces;
+using MeetingRoomScheduling.Infrastructure.Context;
+
+namespace MeetingRoomScheduling.Infrastructure.Repositories
+{
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
