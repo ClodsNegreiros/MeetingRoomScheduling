de443667f5f1bdaf7c64bceed7b098193771db1d
ClodsNegreiros <clodsn@gmail.com>
2025-04-03 20:13:03 -0300

    feat: implementado BookingRepository com métodos necessários (create, update, getById)

---
+++ MeetingRoomScheduling.Domain/Interfaces/IBookingRepository.cs
@@ -0,0 +1,11 @@
+﻿using MeetingRoomScheduling.Domain.Entities;
+
+namespace MeetingRoomScheduling.Domain.Interfaces
+{
+    public interface IBookingRepository
+    {
+        Task<Booking> CreateAsync(Booking booking);
+        Task<Booking> GetById(int id);
+        Task<Booking> UpdateAsync(Booking booking);
+    }
+}
---
+++ MeetingRoomScheduling.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,39 @@
+﻿using MeetingRoomScheduling.Domain.Entities;
+using MeetingRoomScheduling.Domain.Interfaces;
+using MeetingRoomScheduling.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingRoomScheduling.Infrastructure.Repositories
+{
+    public class BookingRepository :  IBookingRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Booking> CreateAsync(Booking booking)
+        {
+            await _context.Bookings.AddAsync(booking);
+            await _context.SaveChangesAsync();
+
+            return booking;
+        }
+
+        public async Task<Booking> UpdateAsync(Booking booking)
+        {
+            _context.Bookings.Update(booking);
+            await _context.SaveChangesAsync();
+
+            return booking;
+        }
+
+        public async Task<Booking> GetById(int id)
+        {
+            return await _context.Bookings
+                .Where(room => room.Id == id)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
